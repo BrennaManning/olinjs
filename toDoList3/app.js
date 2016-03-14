7fce6b0f58bf1cf4a8fbb0d3e33f275eb10daf92
BrennaManning <brenna.manning@students.olin.edu>
2016-03-14 19:50:18 -0400

    finished todo app

--- toDoList3/app.js
+++ toDoList3/app.js
@@ -1,12 +1,14 @@
-var express  = require('express');
-var index = require('./routes/index.js');                               // create our app w/ express
-var mongoose = require('mongoose');                     // mongoose for mongodb
-var morgan = require('morgan');             // log requests to the console (express4)
-var bodyParser = require('body-parser');    // pull information from HTML POST (express4)
-var methodOverride = require('method-override'); // simulate DELETE and PUT (express4)
-var path = require('path');
+//require dependencies
+var express  = require('express');	//express app
+var index = require('./routes/index.js'); //routes   
+var mongoose = require('mongoose'); //mongodb                     
+var morgan = require('morgan');            
+var bodyParser = require('body-parser');    
+var methodOverride = require('method-override'); 
+var path = require('path'); // for path.join
 var app = express();
 
+//database
 mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost/todo');
 
 var db = mongoose.connection;
@@ -14,12 +16,15 @@ db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
   console.log('connected to database');
 });    
-app.use(express.static(__dirname + '/public'));                 // set the static files location /public/img will be /img for users
-app.use(morgan('dev'));                                         // log every request to the console
-app.use(bodyParser.urlencoded({'extended':'true'}));            // parse application/x-www-form-urlencoded
-app.use(bodyParser.json());                                     // parse application/json
-app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
+
+//use
+app.use(express.static(__dirname + '/public'));               
+app.use(morgan('dev'));                                         
+app.use(bodyParser.urlencoded({'extended':'true'}));           
+app.use(bodyParser.json());                                     
+app.use(bodyParser.json({ type: 'application/vnd.api+json' })); 
 app.use(methodOverride());
+//requests
 app.get('/api/todos', index);
 app.post('/api/todos', index);
 app.delete('/api/todos/:todo_id', index);
