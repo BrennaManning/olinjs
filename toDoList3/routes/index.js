4d9a80345f5f65e0735ab95e75f64b8f741ca4b2
BrennaManning <brenna.manning@students.olin.edu>
2016-03-14 19:44:22 -0400

    comments

--- toDoList3/routes/index.js
+++ toDoList3/routes/index.js
@@ -1,50 +1,44 @@
+//require dependencies
 var express  = require('express');
-var router = express.Router();                             // create our app w/ express
+var router = express.Router();                          
 var mongoose = require('mongoose'); 
 var Todo = require('../models/todoModel.js')
 
 router.get('/api/todos', function(req, res) {
-
-  // use mongoose to get all todos in the database
+//get all tasks from database
   Todo.find(function(err, todos) {
-    // if there is an error retrieving, send the error. nothing after res.send(err) will execute
+  
     if (err) { res.send(err) }
     res.json(todos); // return all todos in JSON format
   });
 });
 
-// create todo and send back all todos after creation
 router.post('/api/todos', function(req, res) {
-
-  // create a todo, information comes from AJAX request from Angular
+//create a new task
   Todo.create({
     text : req.body.text,
     done : false
   }, function(err, todo) {
     if (err) {res.send(err);}
 
-    // get and return all the todos after you create another
     Todo.find(function(err, todos) {
-      if (err) {res.send(err)} 
+      if (err) {res.send(err)} //send error if error
       res.json(todos);
     });
   });
 
 });
 
-//edit a todo
+//edit  task
 router.put('/api/todos/:todo_id', function(req, res) {
-
-  // console.log(req)
+// update task after editing
   Todo.update({
-    //_id: mongojs.ObjectId(req.body._id)
     _id: req.params.todo_id
   }, {
     text: req.body.text
   }, {}, function(err, todo) {
     if (err) {res.send(err);}
 
-      // get and return all the todos after you edit
   Todo.find(function(err, todos) {
     if (err) {res.send(err); return;} 
     res.json(todos);
@@ -53,12 +47,10 @@ router.put('/api/todos/:todo_id', function(req, res) {
 
 });
 
-//complete a todo
+//complete  task
 router.post('/api/todos/:todo_id', function(req, res) {
 
-  // console.log(req)
   Todo.update({
-    //_id: mongojs.ObjectId(req.body._id)
     _id: req.params.todo_id
   }, {
     done: true
@@ -66,7 +58,6 @@ router.post('/api/todos/:todo_id', function(req, res) {
     console.log('Todo completed!');
     if (err) {res.send(err);}
 
-      // get and return all the todos after you edit
   Todo.find(function(err, todos) {
     if (err) {res.send(err); return;} 
     res.json(todos);
@@ -74,15 +65,13 @@ router.post('/api/todos/:todo_id', function(req, res) {
   });
 
 });
-
-// delete a todo
+//delete a task from the list
 router.delete('/api/todos/:todo_id', function(req, res) {
   Todo.remove({
       _id : req.params.todo_id
   }, function(err, todo) {
     if (err)
       res.send(err);
-    // get and return all the todos after adding new task
     Todo.find(function(err, todos) {
       if (err) {res.send(err)}
       res.json(todos);
